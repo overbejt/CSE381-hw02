78daef2aec2953655775b8ba4233a6ee616adbff
overbejt <overbejt@miamioh.edu>
2019-09-07 09:33:32 -0400

    Skeleton code is completed

--- overbejt_hw2.cpp
+++ overbejt_hw2.cpp
@@ -1,12 +1,14 @@
 /* 
  * File:   overbejt_hw2.cpp
  * Author: Josh Overbeck
+ * Class: CSE 381 Systems II
+ * Assignment: Homework 2
  *
  * Created on September 7, 2019, 9:20 AM
  * Copyright (c) 2019 overbejt@miamioh.edu
  */
 
-#include <cstdlib>
+#include <iostream>
 
 using namespace std;
 
@@ -14,6 +16,7 @@ using namespace std;
  * 
  */
 int main(int argc, char** argv) {
+    cout << "skeleton code generated" << endl;
     return 0;
-}
+}  // End of main method
 
