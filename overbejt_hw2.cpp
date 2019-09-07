b118f5f692ef120c417c74486016079c5e908229
overbejt <overbejt@miamioh.edu>
2019-09-07 09:29:20 -0400

    Added the neccessary files for the project

--- overbejt_hw2.cpp
+++ overbejt_hw2.cpp
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   overbejt_hw2.cpp
  * Author: Josh Overbeck
  *
  * Created on September 7, 2019, 9:20 AM
+ * Copyright (c) 2019 overbejt@miamioh.edu
  */
 
 #include <cstdlib>
@@ -19,7 +14,6 @@ using namespace std;
  * 
  */
 int main(int argc, char** argv) {
-
     return 0;
 }
 
